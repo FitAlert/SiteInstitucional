28b561c32889bb6632c7b3c7f429626d0ba8b63d
PedroSales <pedro.santos013@sptech.school>
2025-04-24 21:04:06 -0300

    arrumando dashboard

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
 
 // Event listener para clicar no botão de menu
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', function ativar() {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
-});
-
-// Event listener para clicar em qualquer parte da tela
-document.addEventListener('click', (event) => {
-    const target = event.target;
-    const isMenuBtn = target.closest('.menu-btn');
-    const isMenu = target.closest('.menu');
-
-    // Se não clicar no botão de menu nem na caixa de navegação, esconde a caixa de navegação
-    if (!isMenuBtn && !isMenu) {
-        menuBtn.classList.remove('active');
-        menu.classList.remove('active');
-    }
 });
