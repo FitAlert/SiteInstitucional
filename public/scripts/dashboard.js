5c68b5f14d38a3459d49cd44047ca4dd368cb69d
PedroSales <pedro.santos013@sptech.school>
2025-05-19 22:05:57 -0300

    Adicionado menu de alertas na página da dashboard

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,3 +1,15 @@
 window.onload = () => {
     span_usuario.innerHTML = sessionStorage.NOME_USUARIO;
 }
+
+const alerta = document.getElementById('li_alerta');
+const boxAlerta = document.getElementById('boxAlerta');
+
+alerta.addEventListener('click', () => {
+    const displayAtual = window.getComputedStyle(boxAlerta).display;
+    if (displayAtual === 'none') {
+        boxAlerta.style.display = 'flex';
+    } else {
+        boxAlerta.style.display = 'none';
+    }
+});
