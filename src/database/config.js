ad464de720aa361be05688b918b8b36702cc92d8
PedroSales <pedro.santos013@sptech.school>
2025-06-09 22:06:50 -0300

    Adicionado legenda nas KPIs geral e dos provadores individuais

--- src/database/config.js
+++ src/database/config.js
@@ -2,22 +2,23 @@ var mysql = require("mysql2");
 
 // CONEXÃO DO BANCO MYSQL SERVER
 
+/*
 var mySqlConfig = {
     host: "localhost", //inserir host
     database: "DB_FitAlert", //inserir banco
     user: "FitAlert", // inserir usuario 
     password: "fitalert", // inserir senha 
     port: 3306
-};
+};*/
 
 
-// var mySqlConfig = {
-//     host: "localhost", //inserir host
-//     database: "DB_FitAlert", //inserir banco
-//     user: "aluno", // inserir usuario 
-//     password: "Sptech#2024", // inserir senha 
-//     port: 3307
-// };
+ var mySqlConfig = {
+     host: "localhost", //inserir host
+     database: "DB_FitAlert", //inserir banco
+     user: "aluno", // inserir usuario 
+     password: "Sptech#2024", // inserir senha 
+     port: 3307
+ };
 
 function executar(instrucao) {
 
